25fe66e871406bfa47060611934ba02fc69c295b
LujainAlsasa <lujainalsasa@gmail.com>
2024-02-15 18:11:21 +0300

    initial commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5"> My Vue TodoApp </h2>
+
+      <!--input-->
+  <div class="d-flex">
+      <input v-model="task" type="text"  placeholder="Enter task"  class ='from-control'>
+      <button  @click = "submitTask" class="btn btn-warning rounded-0"> SUBMIT</button>
+  </div>
+
+  <!--Task Table-->
+  <table class="table table-bordered mt-5">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col" class="text-center">#</th>
+      <th scope="col" class="text-center">#</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <tr v-for="(task,index) in tasks" :key="index">
+      <th scope="row">
+        <span :class="{finished : task.status === 'Finished' }">
+          {{task.name}} 
+        </span>
+      </th>
+      <td style="width: 120px;">
+         
+         <span class="pointer" @click="changeStatus(index)" 
+         :class="{'text-danger' : task.status === 'To-do' ,
+         'text-warning' : task.status === 'In-progress' ,
+         'text-success' : task.status === 'Finished'
+        }"
+         >
+          {{task.status}} 
+        </span>
+      </td>
+      <td>
+        <div class="text-center" @click="editTask(index)">
+          <span class="fa fa-pen" ></span>
+        </div>
+      </td>
+      <td>
+        <div class="text-center" @click="deleteTask(index)">
+          <span class="fa fa-trash"></span>
+        </div>
+      </td>
+    </tr>
+   
+  </tbody>
+</table>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data (){
+    return {
+      task : "",
+      editedTask : null,
+      avalibaleStatus : ['To-do' , 'In-progress' , 'Finished'],
+      tasks: [
+      {
+        name : "stall bananas from the store",
+        status : "To-do"
+      },
+      {
+        name : "eat 1kg of chcolate  in 1 min",
+        status : "In-progress"
+      }
+      ]
+    }
+  },
+  methods : {
+    submitTask (){
+      if(this.task.length === 0) return;
+
+      if(this.editedTask === null){
+      this.tasks.push(
+        {name : this.task,
+          status : "To-do"
+        }
+      )
+      }
+      else{
+        this.tasks[this.editedTask].name = this.task
+        this.editedTask = null;
+
+      }
+      this.task =''
+    },
+    deleteTask(index){
+      this.tasks.splice(index,1);
+    },
+    editTask(index){
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+    changeStatus(index){
+      let newindex = this.avalibaleStatus.indexOf(this.tasks[index].status);
+      if(++newindex > 2) newindex = 0;
+      this.tasks[index].status = this.avalibaleStatus[newindex];
+    }
+  }
+
+}
+</script>
+
+
+<style scoped>
+.pointer{
+  cursor: pointer;
+}
+.finished{
+  text-decoration: line-through;
+}
+</style>
